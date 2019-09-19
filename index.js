e3229e8eac9614792c129cd993c71557c7f72da6
Roman Kozlov <romankozlov20@gmail.com>
2019-09-19 01:30:42 -0700

    First Assignment commit

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-function myFunction() {
-  alert("Hello! I am an alert box!");
+"use strict";
+
+function getDogImage(num) {
+  const url = "https://dog.ceo/api/breeds/image/random/" + num;
+  console.log(url);
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson))
+    .catch(error => alert("Something went wrong. Try again later."));
 }
-console.log("hello");
-myFunction();
+function printValue(val) {
+  if (val >= 1 && val <= 50) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function watchForm() {
+  $("form").submit(event => {
+    event.preventDefault();
+    const results = $("#num").val();
+    if (printValue(results)) {
+      console.log(`Results shown ${results}`);
+      getDogImage(results);
+    } else {
+      alert("Enter number between 1 and 50");
+    }
+  });
+}
+
+$(function() {
+  console.log("App loaded! Waiting for submit!");
+  watchForm();
+});
